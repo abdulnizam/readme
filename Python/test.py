269a317811aef56f9b0f1a24485c7c634b5148ef
Abdul Nizam <33617554+abdulnizam@users.noreply.github.com>
2025-02-07 16:40:12 +0000

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -1,28 +1,24 @@
-import pytest
-from botocore.exceptions import ClientError, NoCredentialsError, ParamValidationError
+from controller.routes import generate_content_router_v1
+from fastapi.middleware.cors import CORSMiddleware
 
-@pytest.fixture(scope="module", autouse=True)
-def setup_mocked_aws_helpers(mock_get_settings_and_boto3):
-    """Apply mocks before importing `aws_helpers`."""
-    
-    # ✅ Unpack mocks
-    mock_get_settings, mock_boto_client = mock_get_settings_and_boto3  
+from fastapi import FastAPI
 
-    # ✅ Import `aws_helpers` AFTER mocks
-    global get_credentials, get_client, retry_if_invalid_settings
-    from model.aws.aws_helpers import get_credentials, get_client, retry_if_invalid_settings  
+description = '''
+This is the content creation microservice
+'''
 
-# ✅ Tests that automatically use the setup
-@pytest.mark.usefixtures("setup_mocked_aws_helpers")
-def test_get_client():
-    """Tests get_client function."""
-    client = get_client("s3")
-    assert client is not None
+origins = ['http://localhost', 'http://localhost:8083']
 
-@pytest.mark.usefixtures("setup_mocked_aws_helpers")
-def test_get_client_failure(mocker):
-    """Tests get_client when credentials retrieval fails."""
-    mocker.patch("model.aws.aws_helpers.get_credentials", side_effect=Exception("Credential error"))
-    
-    with pytest.raises(Exception, match="Credential error"):
-        get_client("s3")
+app = FastAPI(title='generate_content_microservice',
+              description=description
+              )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+app.include_router(generate_content_router_v1, prefix='/v1')
