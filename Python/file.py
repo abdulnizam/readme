c10f166d79082f9f5fc54278f0c67eaaae131db6
Abdul Nizam <33617554+abdulnizam@users.noreply.github.com>
2025-02-25 15:28:44 +0000

    Update file.py

--- Python/file.py
+++ Python/file.py
@@ -1,244 +1,126 @@
 import logging
-from typing import List
-from pydantic import BaseModel
-from fastapi import HTTPException
-from bson import ObjectId
-from model.db_connection import get_connection
-
-SOURCE_CHUNKS_TYPE = "source-documents"  # Default type
-JOURNEY_TYPE_CHUNK_MAPPING = {"continuous-learning-change": "change-documents"}
-
-
-class NamePurposeTopics(BaseModel):
-    name: str
-    purpose: str
-    topics: List[str]
-
+import uuid
+import lorem
+from model.embeddings_model_config import get_bedrock_embeddings
 
 logger = logging.getLogger()
 
 
-async def connect_db():
-    logger.debug("Establishing db connection")
+def pad_chunks(chunks: list[str], chunk_size: int = 2048):
+    """
+    Adds padding to all chunks that are less than a specified chunk size.
+    The padding is lorum ipsum text.
 
-    collection_name = "learning"
+    Parameters:
+    chunks (list[str]): A list of strings representing the chunks.
+    chunk_size (int): The desired size of each chunk.
 
-    db = await get_connection()
+    Returns:
+    list[str]: A list of strings representing the padded chunks.
+    """
 
-    return db[collection_name]
+    padded_chunks = []
+
+    lorem_text = ""
+    while len(lorem_text) < chunk_size:
+        lorem_text += lorem.text()
+
+    for chunk in chunks:
+        if len(chunk) < chunk_size:
+            chunk += "\n"
+            padding_size = chunk_size - len(chunk)
+            padding = lorem_text[:padding_size]
+            padded_chunk = chunk + padding
+        else:
+            padded_chunk = chunk
+        padded_chunks.append(padded_chunk)
+    return padded_chunks
 
 
-async def send_learning_to_db(data: NamePurposeTopics, learning_id: str):
-    logger.debug("Entered send_learning_to_db")
+def chunk_contents(contents: str, chunk_size: int = 2048, overlap: int = 512) -> list:
+    """
+    Split the contents of a string into chunks of a specified size with a specified overlap.
+
+    Args:
+        contents (str): The input string to be chunked.
+        chunk_size (int, optional): The size of each chunk. Default is 2048.
+        overlap (int, optional): The amount of overlap between chunks. Default is 512.
+
+    Returns:
+        list: A list of chunks, each of size chunk_size, with the specified overlap.
+    """
+
+    logger.info("Document chunking started")
     try:
-        learning_data = data.model_dump()
-        col = await connect_db()
-        response = col.update_one(
-            {"_id": ObjectId(learning_id)},
-            {"$set": learning_data},
-            upsert=True,
-        )
-        print(response.upserted_id)
-        # set learning_id to response from upsert
-        if response.upserted_id is not None:
-            learning_id = str(response.upserted_id)
+        if len(contents) < chunk_size:
+            logger.warning("Content size is smaller than the chunk size")
+            return [contents]
 
-        # return learning_id to ui
-        return {"learning_id": learning_id}
+        if chunk_size <= overlap:
+            logger.warning("Chunk size is smaller than overlap")
+            return [contents]
 
-    except Exception as error:
-        logger.error("Error occured in send_learning_to_db: %s", error)
-        raise HTTPException(
-            status_code=500, detail=f"An error occurred: {error}"
-        ) from error
+        chunks = []
+        i = 0
+        while i < len(contents):
+            chunk = contents[i : i + chunk_size]
+            chunks.append(chunk)
+            i += chunk_size - overlap
 
-
-async def send_chunks_to_db(learning_id: str, chunks_list: list, file_type: str):
-    logger.debug("Entered send_chunks_to_db")
-    try:
-        col = await connect_db()
-
-        # Prepare the update operation
-        update = {"$push": {file_type: {"$each": chunks_list}}}
-        # Perform the update operation
-        response = col.update_one({"_id": ObjectId(learning_id)}, update, upsert=False)
-
-        logger.debug(
-            "Chunks successfully inserted for learning_id: %s",
-            learning_id,
-        )
-        return response.modified_count
-
-    except Exception as error:
-        logger.error("Error occured in send_chunks_to_db: %s", error)
-        raise HTTPException(
-            status_code=500, detail=f"An error occurred: {error}"
-        ) from error
-
-
-async def get_all_chunks(learning_id, chunks_type: str):
-    logger.debug("Entered get chunks for content creation")
-    try:
-        col = await connect_db()
-
-        logger.debug("Getting the database entry")
-        response = col.find_one(
-            {"_id": ObjectId(learning_id)}, {chunks_type: 1, "_id": 0}
-        )
-
-        # Return the right list response to get_all_chunks
-        def get_documents(response):
-            if chunks_type in response:
-                return response[chunks_type]
-            else:
-                return None
-
-        if response is None:
-            logger.warning(
-                "No response recieved from the database with learning_id: %s",
-                learning_id,
-            )
-            raise HTTPException(
-                status_code=404,
-                detail="Chunks not found in database with provided ID",
-            )
-
-        chunks = get_documents(response)
-        logger.debug("All chunks ready to send to get relevant chunks")
-
+        logger.info("Chunks being returned")
         return chunks
-
     except Exception as error:
-        logger.error("Error occured in get_all_chunks: %s", error)
-        raise HTTPException(
-            status_code=500, detail=f"An error occurred: {error}"
-        ) from error
+        logger.error("The content could not be split into chunks: %s", error)
+        return [contents]
 
 
-async def get_all_topics(learning_id):
-    logger.debug("Entered get topics for content creation")
+async def chunk_and_vectorise(
+    contents: str,
+    doc_title: str,
+    chunk_size: int = 2048,
+    overlap: int = 512,
+) -> list:
+    """
+    Chunks the input contents and vectorises each chunk using the specified embedding endpoint.
+
+    Args:
+        embedding_endpoint (BedrockEmbeddings): The embedding endpoint to be used for vectorisation.
+        contents (str): The input text content to be chunked and vectorised.
+        doc_title (str): The title of the document.
+        chunk_size (int, optional): The size of each chunk. Defaults to 2048.
+        overlap (int, optional): The amount of overlap between consecutive chunks. Defaults to 512.
+
+    Returns:
+        list: A list of dictionaries, each containing the document ID, document title, chunk ID,
+        raw text, and vector for each chunk.
+    """
+    logger.info("Chunking and vectorisation")
+
     try:
-        col = await connect_db()
+        chunks_list = []
+        chunks_raw = chunk_contents(contents, chunk_size, overlap)
 
-        logger.debug("Getting the database entry")
+        logger.info("Chunks now being vectorised")
+        embeddings = get_bedrock_embeddings()
 
-        response = col.find_one({"_id": ObjectId(learning_id)}, {"topics": 1, "_id": 0})
+        chunks_vectorised = embeddings.embed_documents(
+            pad_chunks(chunks_raw)
+        )  # Pad out the chunks before embedding -> better performance
 
-        # Return the right list response to get_all_topics
-        def get_documents(response):
-            if "topics" in response:
-                return response["topics"]
-            else:
-                return None
+        doc_id = str(uuid.uuid4())
 
-        if response is None:
-            logger.warning(
-                "No response recieved from the database with learning_id: %s",
-                learning_id,
-            )
-            raise HTTPException(status_code=404, detail="Topics not found in database")
-
-        topics = get_documents(response)
-        logger.debug("All topics ready to send to get relevant chunks")
-
-        return topics
-
-    except Exception as error:
-        logger.error("Error occured in get_all_topics: %s", error)
-        raise HTTPException(
-            status_code=500, detail=f"An error occurred: {error}"
-        ) from error
-
-
-async def get_name_from_datbase(learning_id):
-    logger.debug("Entered get name from database")
-    try:
-        col = await connect_db()
-
-        logger.debug("Requesting name from database")
-
-        response = col.find_one({"_id": ObjectId(learning_id)}, {"name": 1, "_id": 0})
-
-        logger.debug("Name has been received")
-        string_response = response["name"]
-        return string_response
-    except Exception as error:
-        if response is None:
-            logger.error(
-                "No response recieved from the database with learning_id: %s",
-                learning_id,
-            )
-            raise HTTPException(
-                status_code=400,
-                detail="Learning Name not found in database with ID provided",
-            ) from error
-
-        if learning_id is None:
-            logger.warning("No learning ID has been provdied")
-            raise HTTPException(
-                status_code=400,
-                detail="Learning ID not provided for datbase call",
-            ) from error
-
-        logger.error("Error occured in get_all_topics: %s", error)
-        raise HTTPException(
-            status_code=500, detail=f"An error occurred: {error}"
-        ) from error
-
-
-async def remove_by_learning_id(learning_id: str):
-    logger.debug("Entered remove_by_learning_id with learning_id:  %s", learning_id)
-    try:
-        col = await connect_db()
-
-        response = col.delete_one({"_id": ObjectId(learning_id)})
-
-        if response.deleted_count == 0:
-            logger.warning("No document found with learning_id: %s", learning_id)
-            raise HTTPException(status_code=404, detail="Document not found")
-
-        logger.debug("Successfully removed document with learning_id: %s", learning_id)
-        return {"detail": "Document removed successfully"}
+        for i, (raw_text, vector) in enumerate(zip(chunks_raw, chunks_vectorised)):
+            chunk_obj = {
+                "doc_id": doc_id,
+                "doc_title": doc_title,
+                "chunk_id": i,
+                "raw_text": raw_text,
+                "vector": vector,
+            }
+            chunks_list.append(chunk_obj)
+        logger.info("Chunks have been vectorised")
+        return chunks_list, doc_id
 
     except Exception as error:
-        logger.error("Error occured in remove_by_learning_id: %s", error)
-        raise HTTPException(
-            status_code=500, detail=f"An error occurred: {error}"
-        ) from error
-
-
-async def remove_document_from_source_documents(learning_id: str, document_id: str):
-    logger.debug("Entered remove_document_from_source_documents")
-    try:
-        col = await connect_db()
-        update_result = col.update_one(
-            {"_id": ObjectId(learning_id)},
-            {"$pull": {"source_documents": {"doc_id": document_id}}},
-        )
-
-        if update_result.modified_count == 0:
-            logger.warning(
-                "No document found with learning_id: %s or document_id: %s not in source_documents",
-                learning_id,
-                document_id,
-            )
-            raise HTTPException(
-                status_code=404,
-                detail="Document not found or not in source_documents",
-            )
-
-        logger.debug(
-            "Successfully removed document_id: %s from learning_id: %s",
-            document_id,
-            learning_id,
-        )
-        return {"detail": "Document removed successfully"}
-
-    except Exception as error:
-        logger.error(
-            "Error occurred in remove_document_from_source_documents: %s",
-            error,
-        )
-        raise HTTPException(
-            status_code=500, detail=f"An error occurred: {error}"
-        ) from error
+        logger.error("Error occured in vectorisation and chunking: %s", error)
+        return error
