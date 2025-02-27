d1586f2f0704bf38d12a67aa4dd7c62638136c5b
Abdul Nizam <abdul.nizam@genomicsengland.co.uk>
2025-02-27 11:54:43 +0000

    updated the changes

--- Javascript/test.tsx
+++ Javascript/test.tsx
@@ -1,144 +1,29 @@
-test('should handle addRestyleToList for all contexts', () => {
-    const newContent = { heading: 'Updated Slide', bullet_points: 'Updated Point' };
+test('should handle removeRestyleFromList with an unknown context', () => {
+    const initial = {
+      ...initialState,
+      elearningList: [{ review: 'pending', selectedVersion: 0, versions: [[[{ heading: 'Slide', bullet_points: 'Point' }]]]] },
+    };
   
-    const contexts = [
-      { type: FACILITATOR_POWERPOINT_SCRIPT, key: 'powerPointList' },
-      { type: CONTINUOUS_KNOWLEDGE, key: 'knowledgeCheckList' },
-      { type: KNOWLEDGE_CHECK, key: 'knowledgeCheckList' },
-    ];
+    const newState = generatedContentReducer(
+      initial,
+      removeRestyleFromList({ context: 'UNKNOWN_CONTEXT', reviewIndex: 0, multipleItemsIndex: 0 })
+    );
   
-    contexts.forEach(({ type, key }) => {
-      const initial = {
-        ...initialState,
-        [key]: [
-          {
-            review: 'pending',
-            selectedVersion: 0,
-            versions: [[[{ heading: 'Old Slide', bullet_points: 'Old Point' }]]],
-          },
-        ],
-      };
-  
-      const newState = generatedContentReducer(
-        initial,
-        addRestyleToList({ context: type, reviewIndex: 0, newContent, multipleItemsIndex: 0 })
-      );
-  
-      expect(newState[key][0].versions[0][0]).toContainEqual(newContent);
-    });
+    expect(newState).toEqual(initial);
   });
 
-
-  test('should handle removeRestyleFromList for all contexts', () => {
-    const contexts = [
-      { type: POWERPOINT_SCRIPT, key: 'elearningList' },
-      { type: FACILITATOR_POWERPOINT_SCRIPT, key: 'powerPointList' },
-      { type: CONTINUOUS_KNOWLEDGE, key: 'knowledgeCheckList' },
-      { type: KNOWLEDGE_CHECK, key: 'knowledgeCheckList' },
-    ];
-  
-    contexts.forEach(({ type, key }) => {
-      const initial = {
-        ...initialState,
-        [key]: [
-          {
-            review: 'pending',
-            selectedVersion: 0,
-            versions: [[[{ heading: 'Slide', bullet_points: 'Point' }, { heading: 'Extra Slide', bullet_points: 'Extra' }]]],
-          },
-        ],
-      };
-  
-      const newState = generatedContentReducer(
-        initial,
-        removeRestyleFromList({ context: type, reviewIndex: 0, multipleItemsIndex: 0 })
-      );
-  
-      expect(newState[key][0].versions[0][0].length).toBe(1);
-    });
-  });
-
-
-  test('should handle setEditedContent for all contexts', () => {
+  test('should handle setEditedContent with an unknown context', () => {
     const newContent = { heading: 'Edited Slide', bullet_points: 'Edited Point' };
   
-    const contexts = [
-      { type: CONTINUOUS_TOPICS, key: 'topicList' },
-      { type: TOPIC_OUTLINES, key: 'topicList' },
-      { type: POWERPOINT_SCRIPT, key: 'elearningList' },
-      { type: FACILITATOR_POWERPOINT_SCRIPT, key: 'powerPointList' },
-      { type: CONTINUOUS_KNOWLEDGE, key: 'knowledgeCheckList' },
-      { type: KNOWLEDGE_CHECK, key: 'knowledgeCheckList' },
-      { type: CONTINUOUS_CORE_CONTENT, key: 'continuousList' },
-    ];
+    const initial = {
+      ...initialState,
+      elearningList: [{ review: 'pending', selectedVersion: 0, versions: [[[{ heading: 'Old Slide', bullet_points: 'Old Point' }]]]] },
+    };
   
-    contexts.forEach(({ type, key }) => {
-      const initial = {
-        ...initialState,
-        [key]: [
-          {
-            review: 'pending',
-            selectedVersion: 0,
-            versions: [[[{ heading: 'Old Slide', bullet_points: 'Old Point' }]]],
-          },
-        ],
-      };
+    const newState = generatedContentReducer(
+      initial,
+      setEditedContent({ context: 'UNKNOWN_CONTEXT', reviewIndex: 0, newContent, multipleItemsIndex: 0 })
+    );
   
-      const newState = generatedContentReducer(
-        initial,
-        setEditedContent({ context: type, reviewIndex: 0, newContent, multipleItemsIndex: 0 })
-      );
-  
-      expect(newState[key][0].versions[0][0][0]).toEqual(newContent);
-    });
-  });
-
-  test('should handle setEditContextListReviewComplete for all contexts', () => {
-    const contexts = [
-      { type: POWERPOINT_SCRIPT, key: 'elearningList' },
-      { type: FACILITATOR_POWERPOINT_SCRIPT, key: 'powerPointList' },
-      { type: KNOWLEDGE_CHECK, key: 'knowledgeCheckList' },
-      { type: CONTINUOUS_CORE_CONTENT, key: 'continuousList' },
-    ];
-  
-    contexts.forEach(({ type, key }) => {
-      const initial = {
-        ...initialState,
-        [key]: [{ review: 'pending', selectedVersion: 0, versions: [[[]]] }],
-      };
-  
-      const newState = generatedContentReducer(
-        initial,
-        setEditContextListReviewComplete({ context: type, reviewIndex: 0 })
-      );
-  
-      expect(newState[key][0].review).toBe('completed');
-    });
-  });
-
-
-  test('should handle addNewListItem for all contexts', () => {
-    const newItem = [{ heading: 'New Slide', bullet_points: 'New Point' }];
-  
-    const contexts = [
-      { type: POWERPOINT_SCRIPT, key: 'elearningList' },
-      { type: FACILITATOR_POWERPOINT_SCRIPT, key: 'powerPointList' },
-      { type: CONTINUOUS_KNOWLEDGE, key: 'knowledgeCheckList' },
-      { type: KNOWLEDGE_CHECK, key: 'knowledgeCheckList' },
-      { type: CONTINUOUS_CORE_CONTENT, key: 'continuousList' },
-    ];
-  
-    contexts.forEach(({ type, key }) => {
-      const initial = {
-        ...initialState,
-        [key]: [],
-      };
-  
-      const newState = generatedContentReducer(
-        initial,
-        addNewListItem({ context: type, list: newItem })
-      );
-  
-      expect(newState[key]).toEqual(newItem);
-    });
+    expect(newState).toEqual(initial);
   });
