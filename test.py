db58556c3ccaeda5692b51ed96fc5177dd681457
Abdul Nizam <33617554+abdulnizam@users.noreply.github.com>
2025-04-02 11:05:38 +0100

    Update test.py

--- test.py
+++ test.py
@@ -25,3 +25,31 @@ def check_db_connection():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+DB_CONFIG = {
+    "dbname": "postgres",
+    "user": "citus",
+    "password": "jR[=m9DVoxV>]}a?",
+    "host": "c-postgresqldb-replica-uks-devt1-sd-sib-dwpask.auprpts4zhpysg.privatelink.postgres.cosmos.azure.com",
+    "port": 5432,
+    "sslmode": "require"
+}
+
+
+
+def db_connection(): 
+    con = psycopg2.connect(DB_CONFIG)
+    con.close()
+    return con.get_parameter_status("ssl_version"), con.get_parameter_status("ssl_ciper")  
+
+
+@app.route('/health/db')
+def check_db_connection():
+    try:
+        ssl_version, ssl_ciper = db_connection()
+        return jsonify({'status': 'success', 'message': 'Connected to database', 'ssl_version': ssl_version, "ssl_ciper": ssl_ciper}), 200
+    except OperationalError as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500
