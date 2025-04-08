dd74b68f4e25073a069d5e5931ccbb0eef1a0d66
Abdul Nizam <33617554+abdulnizam@users.noreply.github.com>
2025-04-08 19:53:32 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-from datetime import datetime, timedelta
-from typing import Tuple
-
-def normalize_date_range(start_date_str: str, end_date_str: str) -> Tuple[datetime, datetime]:
-    # Parse ISO strings to datetime objects
-    start_date = datetime.fromisoformat(start_date_str.replace('Z', '+00:00'))
-    end_date = datetime.fromisoformat(end_date_str.replace('Z', '+00:00'))
-
-    # If the dates are the same, extend end date by 1 day
-    if start_date == end_date:
-        end_date += timedelta(days=1)
-
-    return start_date, end_date
+  useEffect(() => {
+    const fetchData = async () => {
+        const now = convertDateToParts(new Date());
+        const current_date = convertDateToISO(now.day, now.month, now.year);
+        try {
+            const res: messagesResponseType[] = await getMessages(current_date, current_date);
+            setchatMessages(res)
+        } catch (err) {
+            console.error('Error fetching data:', err)
+        }
+    }
+    fetchData();
+  }, [])
