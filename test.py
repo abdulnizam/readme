41b083a4e33eca88c24180fa929cba723fa950de
Abdul Nizam <33617554+abdulnizam@users.noreply.github.com>
2025-04-08 19:36:24 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,49 @@
-type DateParts = {
-  day: string
-  month: string
-  year: string
-}
+ message_links = await db.messageusers.find_many(
+            where={
+                'user_id': user.id,
+                'message': {
+                    'created_at': {
+                        'gte': start_date,
+                        'lte': end_date
+                    },
+                    'requestType': {
+                        'is_active': True
+                    },
+                    'messageType': {
+                        'is_active': True
+                    }
+                }
+            },
+            order={
+                'message': {
+                    'created_at': 'desc'
+                }
+            },
+            include={
+                'message': {
+                    'include': {
+                        'country': True,
+                        'requestType': True,
+                        'messageType': True,
+                        'dataSource': True,
+                        'messageCitations': True,
+                        'feedback': {
+                            'include': {
+                                'messageFeedback': {
+                                    'include': {
+                                        'feedbackType': True
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        )
 
-export const convertDateToParts = (date: Date): DateParts => {
-  return {
-    day: String(date.getDate()).padStart(2, '0'),
-    month: String(date.getMonth() + 1).padStart(2, '0'), // months are 0-based
-    year: String(date.getFullYear())
-  }
+
+
+{
+    "start_date": "2025-04-07T00:00:00.000Z",
+    "end_date": "2025-04-09T00:00:00.000Z"
 }
