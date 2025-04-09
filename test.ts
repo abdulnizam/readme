a21583c6065188ee55eca5d4c7653a3ef332af54
Abdul Nizam <33617554+abdulnizam@users.noreply.github.com>
2025-04-09 17:05:09 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,2 +1,44 @@
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
+message_links = await db.messageusers.find_many(
+            where={
+                'user_id': user.id,
+                'message': {
+                    'created_at': {
+                        'gte': start_date,
+                        'lte': adjusted_end_date
+                    },
+                    'requestType': {
+                        'is_active': True
+                    },
+                    'messageType': {
+                        'is_active': True
+                    }
+                }
+            },
+            order={
+                'message': {
+                    'created_at': 'desc'
+                }
+            },
+            skip=skip,
+            take=page_size,
+            include={
+                'message': {
+                    'include': {
+                        'country': True,
+                        'requestType': True,
+                        'messageType': True,
+                        'dataSource': True,
+                        'messageCitations': True,
+                        'feedback': {
+                            'include': {
+                                'messageFeedback': {
+                                    'include': {
+                                        'feedbackType': True
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        )
