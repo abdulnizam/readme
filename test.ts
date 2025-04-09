e4ff8f0624cc752f5ff0cbf9857658b43818cb46
Abdul Nizam <33617554+abdulnizam@users.noreply.github.com>
2025-04-09 21:16:17 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,13 +1,4 @@
-if (!sessionStorage.getItem("session_id")) {
-    sessionStorage.setItem("session_id", crypto.randomUUID());
-  }
-
-
-
-  model Message {
-    id        Int     @id @default(autoincrement())
-    question  String
-    response  String
-    ...
-    session_id String // 👈 Add this to group messages
-  }
+Subsequent property declarations must have the same type.  Property 'randomUUID' must be of type '{ (): `${string}-${string}-${string}-${string}-${string}`; (): string; }', but here has type '() => any'.ts(2717)
+lib.dom.d.ts(6446, 5): 'randomUUID' was also declared here.
+(method) Crypto.randomUUID(): `${string}-${string}-${string}-${string}-${string}` (+1 overload)
+Available only in secure contexts.
