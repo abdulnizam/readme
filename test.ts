f0ad55e0c10a18ec14d8cb2548242d651895f014
Abdul Nizam <abdul.nizam@genomicsengland.co.uk>
2025-04-09 16:50:24 +0100

    updated the chnages

--- test.ts
+++ test.ts
@@ -1,9 +1,2 @@
-Error: P3016
-
-The fallback method for database resets failed, meaning Migrate could not clean up the database entirely. Original error: 
-ERROR: must be owner of view citus_tables
-   0: sql_schema_connector::best_effort_reset
-           with namespaces=None
-             at schema-engine/connectors/sql-schema-connector/src/lib.rs:357
-   1: schema_core::state::Reset
-             at schema-engine/core/src/state.rs:453
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
