373d889e46125f16f61fc501f5b9a549dd5fe811
Abdul Nizam <33617554+abdulnizam@users.noreply.github.com>
2025-04-09 16:42:26 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,109 +1,134 @@
-"use client";
+// To create/edit a table, define/edit the model and run `prisma db push`
+// To update the Prisma client types with db changes, run `prisma generate`
+// To view the tables, run `prisma studio` or use pgAdmin
 
-import React from "react";
-import styles from "./Pagination.module.css";
+datasource db {
+  url      = env("POSTGRES_CONN_STRING")
+  provider = "postgresql"
+}
 
-export default function Pagination({
-  currentPage,
-  totalPages,
-  onPageChange
-}: {
-  currentPage: number
-  totalPages: number
-  onPageChange: (page: number) => void
-}) {
-  const isFirstPage = currentPage === 1;
-  const isLastPage = currentPage === totalPages;
+generator client {
+  provider = "prisma-client-py"
+  recursive_type_depth = -1
+}
 
-  return (
-    <div className={styles.Pagination}>
-      <div className={styles.InnerPagination}>
-        <nav className="govuk-pagination" aria-label="Pagination">
-          
-          {/* Previous Button */}
-          <div className={`govuk-pagination__prev ${isFirstPage ? styles.disabledLink : ""}`}>
-            <a
-              href="#"
-              className={`govuk-link govuk-pagination__link ${isFirstPage ? styles.disabledLink : ""}`}
-              aria-disabled={isFirstPage}
-              onClick={(e) => {
-                e.preventDefault();
-                if (!isFirstPage) {
-                  onPageChange(currentPage - 1);
-                }
-              }}
-            >
-              <svg
-                className="govuk-pagination__icon govuk-pagination__icon--prev"
-                xmlns="http://www.w3.org/2000/svg"
-                height="13"
-                width="15"
-                aria-hidden="true"
-                focusable="false"
-                viewBox="0 0 15 13"
-              >
-                <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"></path>
-              </svg>
-              <span className="govuk-pagination__link-title">
-                Previous<span className="govuk-visually-hidden"> page</span>
-              </span>
-            </a>
-          </div>
 
-          {/* Page List */}
-          <ul className="govuk-pagination__list">
-            {Array.from({ length: totalPages }, (_, i) => i + 1).map(page => (
-              <li
-                key={page}
-                className={`govuk-pagination__item ${currentPage === page ? `govuk-pagination__item--current ${styles.navActivate}` : ''}`}
-              >
-                <a
-                  href="#"
-                  className="govuk-link govuk-pagination__link"
-                  aria-label={`Page ${page}`}
-                  aria-current={currentPage === page ? "page" : undefined}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    onPageChange(page);
-                  }}
-                >
-                  {page}
-                </a>
-              </li>
-            ))}
-          </ul>
+model Message {
+  id Int @id @default(autoincrement())
+  question String
+  response String
+  previous_chat_history Json @default("{}")
+  delete_date DateTime @default(dbgenerated("(NOW() + INTERVAL '2 years')"))
+  created_at DateTime @default(now())
+  // FKs
+  country_id Int
+  request_type_id Int
+  message_type_id Int
+  data_source_id Int
+  user_id Int
+  // Relations
+  country Country @relation(fields: [country_id], references: [id])
+  requestType RequestType @relation(fields: [request_type_id], references: [id])
+  messageType MessageType @relation(fields: [message_type_id], references: [id])
+  dataSource DataSource @relation(fields: [data_source_id], references: [id])
+  user Users @relation(fields: [user_id], references: [id])
+  
+  feedback Feedback[]
+  messageCitations MessageCitations[]
 
-          {/* Next Button */}
-          <div className={`govuk-pagination__next ${isLastPage ? styles.disabledLink : ""}`}>
-            <a
-              href="#"
-              className={`govuk-link govuk-pagination__link ${isLastPage ? styles.disabledLink : ""}`}
-              aria-disabled={isLastPage}
-              onClick={(e) => {
-                e.preventDefault();
-                if (!isLastPage) {
-                  onPageChange(currentPage + 1);
-                }
-              }}
-            >
-              <span className="govuk-pagination__link-title">
-                Next<span className="govuk-visually-hidden"> page</span>
-              </span>
-              <svg
-                className="govuk-pagination__icon govuk-pagination__icon--next"
-                xmlns="http://www.w3.org/2000/svg"
-                height="13"
-                width="15"
-                aria-hidden="true"
-                focusable="false"
-                viewBox="0 0 15 13"
-              >
-                <path d="m8.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z"></path>
-              </svg>
-            </a>
-          </div>
-        </nav>
-      </div>
-    </div>
-  );
+  @@id([user_id])
+}
+
+model MessageFeedback {
+  feedback_id Int
+  feedback_type_id Int
+  // Relations
+  feedback Feedback @relation(fields: [feedback_id], references: [id])
+  feedbackType FeedbackType @relation(fields: [feedback_type_id], references: [id])
+  // composite primary key
+  @@id([feedback_type_id, feedback_id])
+}
+
+model MessageCitations {
+  message_id Int
+  title String
+  url String
+  source_extracts String
+  // Relations
+  message Message @relation(fields: [message_id], references: [id])
+
+  @@id([message_id, title])
+}
+
+
+model Feedback {
+    id Int @id @default(autoincrement())
+    feedback_free_text String
+    created_at DateTime @default(now())
+    // FKs
+    message_id Int
+    // Relations
+    message Message @relation(fields: [message_id], references: [id])
+    messageFeedback MessageFeedback[]
+}
+
+model Users {
+  id Int @id @default(autoincrement())
+  given_name String
+  family_name String
+  job_centre String
+  unique_identifier String @unique
+  created_at DateTime @default(now())
+
+  messages Message[]
+}
+
+model Country {
+  id Int @id @default(autoincrement())
+  name String
+  is_active Boolean @default(true)
+  created_at DateTime @default(now())
+  updated_at DateTime @default(now())
+  // Relations
+  messages Message[]
+}
+
+model MessageType {
+  id Int @id @default(autoincrement())
+  name String
+  is_active Boolean @default(true)
+  created_at DateTime @default(now())
+  updated_at DateTime @default(now())
+  // Relations
+  messages Message[]
+}
+
+
+model RequestType {
+  id Int @id @default(autoincrement())
+  name String
+  is_active Boolean @default(true)
+  created_at DateTime @default(now())
+  updated_at DateTime @default(now())
+  // Relations
+  messages Message[]
+}
+
+model FeedbackType {
+  id Int @id @default(autoincrement())
+  name String
+  is_active Boolean @default(true)
+  created_at DateTime @default(now())
+  updated_at DateTime @default(now())
+  // Relations
+  messageFeedback MessageFeedback[]
+}
+
+model DataSource {
+  id Int @id @default(autoincrement())
+  name String
+  is_active Boolean @default(true)
+  created_at DateTime @default(now())
+  updated_at DateTime @default(now())
+  messages Message[]
 }
