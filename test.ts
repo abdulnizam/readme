068a08679cccd3d707823d626c384d1fe1b3a1f9
Abdul Nizam <abdul.nizam@genomicsengland.co.uk>
2025-04-09 21:17:39 +0100

    updated the chnages

--- test.ts
+++ test.ts
@@ -1,4 +1,1 @@
-Subsequent property declarations must have the same type.  Property 'randomUUID' must be of type '{ (): `${string}-${string}-${string}-${string}-${string}`; (): string; }', but here has type '() => any'.ts(2717)
-lib.dom.d.ts(6446, 5): 'randomUUID' was also declared here.
-(method) Crypto.randomUUID(): `${string}-${string}-${string}-${string}-${string}` (+1 overload)
-Available only in secure contexts.
+(global.crypto.randomUUID as jest.Mock) = jest.fn(() => 'mock-session-id-123');
