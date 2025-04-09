b379cec975318f7d3009002225e49ba250ec1279
Abdul Nizam <abdul.nizam@genomicsengland.co.uk>
2025-04-09 17:06:45 +0100

    updated the chnages

--- test.ts
+++ test.ts
@@ -1,44 +1,36 @@
-message_links = await db.messageusers.find_many(
-            where={
-                'user_id': user.id,
-                'message': {
-                    'created_at': {
-                        'gte': start_date,
-                        'lte': adjusted_end_date
-                    },
-                    'requestType': {
-                        'is_active': True
-                    },
-                    'messageType': {
-                        'is_active': True
-                    }
-                }
-            },
-            order={
-                'message': {
-                    'created_at': 'desc'
-                }
-            },
-            skip=skip,
-            take=page_size,
-            include={
-                'message': {
+message_links = await db.message.find_many(
+    where={
+        'user_id': user.id,
+        'created_at': {
+            'gte': start_date,
+            'lt': adjusted_end_date
+        },
+        'requestType': {
+            'is_active': True
+        },
+        'messageType': {
+            'is_active': True
+        }
+    },
+    order={
+        'created_at': 'desc'
+    },
+    skip=skip,
+    take=page_size,
+    include={
+        'country': True,
+        'requestType': True,
+        'messageType': True,
+        'dataSource': True,
+        'messageCitations': True,
+        'feedback': {
+            'include': {
+                'messageFeedback': {
                     'include': {
-                        'country': True,
-                        'requestType': True,
-                        'messageType': True,
-                        'dataSource': True,
-                        'messageCitations': True,
-                        'feedback': {
-                            'include': {
-                                'messageFeedback': {
-                                    'include': {
-                                        'feedbackType': True
-                                    }
-                                }
-                            }
-                        }
+                        'feedbackType': True
                     }
                 }
             }
-        )
+        }
+    }
+)
